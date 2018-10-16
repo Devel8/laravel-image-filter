298efaf97a42567c5192c398098b9437a9030d4b
Ismael <ismael.franco.lizana@gmail.com>
2018-10-16 14:08:25 +0200

    Initialize laravel package structure and some files

---
+++ src/ImageFilterProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Devel8\LaravelImageFilter;
+
+use Illuminate\Support\ServiceProvider;
+
+class ImageFilterProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/image-filter.php' => config_path('image-filter.php'),
+        ], 'config');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/image-filter.php', 'image-filter'
+        );
+    }
+}
